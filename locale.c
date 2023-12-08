sysfce2/perl

locale.c: Reorder some statements in a switch()
This unifies the common text in two case: statements, and makes all of
them do their business in the same order.

This makes it easier to compare and contrast the actions of the various
case:s

locale.c
@@ -5544,31 +5544,30 @@ S_my_localeconv(pTHX_ const int item)
 #    ifdef USE_LOCALE_NUMERIC
 
           case RADIXCHAR:
-            locale = numeric_locale = PL_numeric_name;
-            index_bits = OFFSET_TO_BIT(NUMERIC_OFFSET);
             strings[NUMERIC_OFFSET] = DECIMAL_POINT_ADDRESS;
-            integers = NULL;
-            break;
+            goto numeric_common;
 
           case THOUSEP:
+            strings[NUMERIC_OFFSET] = thousands_sep_string;
+
+          numeric_common:
+            integers = NULL;
             index_bits = OFFSET_TO_BIT(NUMERIC_OFFSET);
             locale = numeric_locale = PL_numeric_name;
-            strings[NUMERIC_OFFSET] = thousands_sep_string;
-            integers = NULL;
             break;
 
 #    endif
 #    ifdef USE_LOCALE_MONETARY
 
           case CRNCYSTR:
-            index_bits = OFFSET_TO_BIT(MONETARY_OFFSET);
-            locale = monetary_locale = querylocale_i(LC_MONETARY_INDEX_);
-
             /* This item needs the values for both the currency symbol, and
              * another one used to construct the nl_langino()-compatible
              * return. */
             strings[MONETARY_OFFSET] = CURRENCY_SYMBOL_ADDRESS;
             integers = P_CS_PRECEDES_ADDRESS;
+
+            index_bits = OFFSET_TO_BIT(MONETARY_OFFSET);
+            locale = monetary_locale = querylocale_i(LC_MONETARY_INDEX_);
             break;
 
 #    endif

guest20/perl5

locale.c
@@ -5154,31 +5154,30 @@ S_my_localeconv(pTHX_ const int item)
 #    ifdef USE_LOCALE_NUMERIC
 
           case RADIXCHAR:
-            locale = numeric_locale = PL_numeric_name;
-            index_bits = OFFSET_TO_BIT(NUMERIC_OFFSET);
             strings[NUMERIC_OFFSET] = DECIMAL_POINT_ADDRESS;
-            integers = NULL;
-            break;
+            goto numeric_common;
 
           case THOUSEP:
+            strings[NUMERIC_OFFSET] = thousands_sep_string;
+
+          numeric_common:
+            integers = NULL;
             index_bits = OFFSET_TO_BIT(NUMERIC_OFFSET);
             locale = numeric_locale = PL_numeric_name;
-            strings[NUMERIC_OFFSET] = thousands_sep_string;
-            integers = NULL;
             break;
 
 #    endif
 #    ifdef USE_LOCALE_MONETARY
 
           case CRNCYSTR:
-            index_bits = OFFSET_TO_BIT(MONETARY_OFFSET);
-            locale = monetary_locale = querylocale_c(LC_MONETARY);
-
             /* This item needs the values for both the currency symbol, and
              * another one used to construct the nl_langino()-compatible
              * return. */
             strings[MONETARY_OFFSET] = CURRENCY_SYMBOL_ADDRESS;
             integers = P_CS_PRECEDES_ADDRESS;
+
+            index_bits = OFFSET_TO_BIT(MONETARY_OFFSET);
+            locale = monetary_locale = querylocale_c(LC_MONETARY);
             break;
 
 #    endif